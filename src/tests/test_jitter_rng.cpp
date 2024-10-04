6596ba377408b265d7849dc70c56a34cdff3ce7b
Dirk Zimmermann <dz@planck.security>
2024-10-04 19:29:29 +0200

    Make AutoSeeded_RNG use the jitter entropy source (test)

--- src/tests/test_jitter_rng.cpp
+++ src/tests/test_jitter_rng.cpp
@@ -37,7 +37,7 @@ class Jitter_RNG_Tests final : public Test {
       Test::Result test_entropy_source() {
          Botan::Entropy_Sources entropy_sources;
          entropy_sources.add_source(Botan::Entropy_Source::create("jitter"));
-         Botan::AutoSeeded_RNG rng{Botan::system_rng(), entropy_sources};
+         Botan::AutoSeeded_RNG rng{entropy_sources};
          std::vector<uint8_t> buf(512);
          rng.randomize(buf.data(), buf.size());
 
